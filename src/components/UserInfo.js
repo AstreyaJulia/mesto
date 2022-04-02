08510e67f46e4f25cb9e8e713e1f20ae6e4c2cbd
Юлия Латышева <58434576+AstreyaJulia@users.noreply.github.com>
2022-04-02 19:07:51 +0300

    upd: в UserInfo добавлен аватар

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -3,10 +3,12 @@ export class UserInfo {
 
   /** Конструктор
    * @param profileTitle - селектор элемента с именем пользователя на странице
+   * @param avatar -  селектор изображения с аватаром пользователя на странице
    * @param profileSubtitle - селектор элемента с подписью пользователя на странице */
-  constructor({profileTitle, profileSubtitle}) {
+  constructor({profileTitle, profileSubtitle, profileAvatar}) {
     this._title = document.querySelector(profileTitle);
     this._subtitle = document.querySelector(profileSubtitle);
+    this._avatar = document.querySelector(profileAvatar);
   }
 
   /** Получает информацию о пользователе из разметки
@@ -15,6 +17,7 @@ export class UserInfo {
     this._info = {
       title: this._title.textContent,
       subtitle: this._subtitle.textContent,
+      avatar: this._avatar.src
     };
     return this._info;
   }
@@ -24,5 +27,6 @@ export class UserInfo {
   setUserInfo(info) {
     this._title.textContent = info.name;
     this._subtitle.textContent = info.title;
+    this._avatar.src = info.avatar;
   }
 }
