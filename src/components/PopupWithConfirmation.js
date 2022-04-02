5ebae7f2ad05896e0eb92f06e360e564d6c425c9
Юлия Латышева <58434576+AstreyaJulia@users.noreply.github.com>
2022-04-02 19:16:36 +0300

    upd: в PopupWithConfirmation добавлен хендлер по submit формы удаления

--- src/components/PopupWithConfirmation.js
+++ src/components/PopupWithConfirmation.js
@@ -1,11 +1,36 @@
 import {Popup} from "./Popup.js";
 
 export class PopupWithConfirmation extends Popup {
-  constructor(popupSelector) {
+  /** Всплывашка с подтверждением, должна присутствовать form с селектором .popup__form
+   * @param popupSelector - селектор всплывашки
+   * @param handleSubmitDelete - хендлер по отправке формы */
+  constructor(popupSelector, {handleSubmitDelete}) {
     super(popupSelector);
+    this._handleSubmitDelete = handleSubmitDelete;
+    this._form = this._popup.querySelector('.popup__form');
   }
 
-  open() {
-    super.open();
+  /** Установка прослушивателя отправки формы */
+  setEventListeners() {
+    super.setEventListeners();
+    this._form.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      this._handleSubmitDelete(this._idCard, this._cardElement);
+      this.close();
+    })
   }
+
+  getCard(id, element) {
+    this._clear();
+    this._idCard = id;
+    this._cardElement = element;
+  }
+
+  /** Очистка id карточки, элемента карточки
+   * @private */
+  _clear() {
+    this._idCard = '';
+    this._cardElement = '';
+  }
+
 }
